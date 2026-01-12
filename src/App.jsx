5ed3cd9639cbb6335c0680d695fe5704e4a56dad
athamovic08 <sadykovatham986@gmail.com>
2026-01-12 16:44:33 +0500

    lolo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Logo from "./assets/logo.png"
 
 const App = () => {
   return (
     <div>
-      
+         <nav>
+          <ul>
+              <img src={Logo} alt="" />
+              <h1>Product</h1>
+          </ul>
+          <ul> 
+            <li><a href="">Product</a></li>
+            <li><a href="">Costumers</a></li>
+            <li><a href="">Pricing</a></li>
+            <li><a href="">Sources</a></li>
+            <button>Sign in</button>
+            <button>Sign up</button>
+            </ul>
+         </nav>
     </div>
   )
 }
