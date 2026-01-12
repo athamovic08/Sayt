6586a3334d6d8a857d0cfedc2bb28533133d8414
SAYKO <saidalomirsabitov@gmail.com>
2026-01-12 16:47:52 +0500

    salam

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,26 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      
+      <div className='flex justify-center items-center gap-9'>
+
+        <div>
+        <h2 className='text-xl'>Effortless Validation for</h2>
+        <h1 className='text-2xl font-bold'>Management</h1>
+        </div>
+
+        <div className='flex justify-center items-center flex-col gap-4'>
+        <div>
+          <h1 className='text-xl'>Accessory makers</h1>
+          <p className='text-xl'>While most people enjoy casino gambling, sports betting, <br /> lottery and bingo playing for the fun</p>
+        </div>
+        <div>
+          <h2 className='text-xl'>Alterationists</h2>
+          <p className='text-xl'>If you are looking for a new way to promote your business <br /> that won’t cost you more money, </p>
+        </div></div>
+<div>
+  <img src="Sayt/src/assets/amico.png" alt="" />
+</div>
+      </div>
     </div>
   )
 }
